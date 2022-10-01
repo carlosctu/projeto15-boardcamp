a386649c8aee8899f1e5d6ef5775191df81b9eb5
carlos_casanova <carlosctu94@gmail.com>
2022-09-30 23:41:18 -0300

    [FEAT] Customers API

--- src/server.js
+++ src/server.js
@@ -63,8 +63,8 @@ server.get("/games", async (req, res) => {
       games = await connection.query(
         `SELECT categories.name AS "categoryName", games.* 
          FROM categories INNER JOIN games ON categories.id = games."categoryId"
-         WHERE games."name" LIKE $1;`,
-        [`${name}%`]
+         WHERE LOWER (games."name") LIKE $1;`,
+        [`${name.toLowerCase()}%`]
       );
     } else {
       games = await connection.query(
@@ -110,6 +110,100 @@ server.post("/games", async (req, res) => {
       [name, image, stockTotal, categoryId, pricePerDay]
     );
     return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+});
+
+const customerSchema = joi.object({
+  name: joi.string().required(),
+  phone: joi.string().min(10).max(11).required(),
+  cpf: joi
+    .string()
+    .pattern(/^[0-9]+$/)
+    .min(11)
+    .max(11)
+    .required(),
+  birthday: joi.string().required(),
+});
+
+server.get("/customers", async (req, res) => {
+  const { cpf } = req.query;
+  let customers;
+  try {
+    if (cpf) {
+      customers = await connection.query(
+        "SELECT * FROM customers WHERE cpf LIKE $1;",
+        [`${cpf.toLowerCase()}%`]
+      );
+    } else {
+      customers = await connection.query("SELECT * FROM customers;");
+    }
+    return res.status(200).send(customers.rows);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+});
+server.get("/customers/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const customer = await connection.query(
+      "SELECT * FROM customers WHERE id = $1;",
+      [id]
+    );
+    if (customer.rowCount == 0) return res.sendStatus(404);
+    return res.status(200).send(customer.rows);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+});
+server.post("/customers", async (req, res) => {
+  const { name, phone, cpf, birthday } = req.body;
+  let validation;
+  validation = customerSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const errors = validation.error.details.map((error) => error.message);
+    return res.status(400).send(errors);
+  }
+
+  try {
+    const cpfExists = await connection.query(
+      "SELECT * FROM customers WHERE cpf = $1;",
+      [cpf]
+    );
+    if (cpfExists.rowCount != 0) return res.sendStatus(409);
+
+    connection.query(
+      "INSERT INTO customers (name, phone, cpf, birthday) values ($1, $2, $3, $4);",
+      [name, phone, cpf, birthday]
+    );
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+});
+server.put("/customers/:id", async (req, res) => {
+  const { name, phone, cpf, birthday } = req.body;
+  const { id } = req.params;
+  if (!id) return res.sendStatus(404);
+
+  let validation;
+  validation = customerSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const errors = validation.error.details.map((error) => error.message);
+    return res.status(400).send(errors);
+  }
+
+  try {
+    await connection.query(
+      "UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;",
+      [name, phone, cpf, birthday, id]
+    );
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
     return res.sendStatus(404);
