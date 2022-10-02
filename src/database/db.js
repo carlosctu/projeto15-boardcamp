b003c532a8a15c9dfded7f8ccd60bd697df6a5ca
carlos_casanova <carlosctu94@gmail.com>
2022-10-02 19:12:11 -0300

    [REFACTOR] Adding the .env path

--- src/database/db.js
+++ src/database/db.js
@@ -2,17 +2,17 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 const { Pool } = pg;
-//dotenv.config();
+dotenv.config();
 
-const connection = new Pool({
-host: "localhost",
-port: 5432,
-user: "postgres",
-password: "123456",
-database: "postgres",
-});
 //const connection = new Pool({
- // connectionString: process.env.DATABASE_URL,
+//host: "localhost",
+//port: 5432,
+//user: "postgres",
+//password: "123456",
+//database: "postgres",
 //});
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 export default connection;
